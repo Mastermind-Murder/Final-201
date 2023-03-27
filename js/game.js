e8d5d76a70baa4a59ff768d354bb929a8c8f4e5c
Isabel Sopha <i.sopha94@gmail.com>
2023-03-27 11:35:51 -0700

    added card interactivity

---
+++ js/game.js
@@ -0,0 +1,9 @@
+//This flips the cards
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('flip');
+  });
+});

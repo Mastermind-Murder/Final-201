1ef9227b08b86cf558c3d27f7a8bdf9af3c5f361
Isabel Sopha <i.sopha94@gmail.com>
2023-03-27 20:44:56 -0700

    a bunch of css/html changes

--- js/game.js
+++
@@ -1,9 +0,0 @@
-//This flips the cards
-
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    card.classList.toggle('flip');
-  });
-});

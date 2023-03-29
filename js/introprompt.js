5d8b77ea3ce1bffb16e4ac44db9c01136431fb58
Hayden James Cooper <haydencpr95@gmail.com>
2023-03-29 16:56:45 -0400

    Jumpscare

---
+++ js/introprompt.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let container = document.getElementById('image-container');
+function quiz() {
+  let entrance = prompt('Welcome to the Murder And Mayhem, Are you ready to solve the unsolved mystery that await you?').toLowerCase();
+  if ( entrance === 'yes') {
+    alert('Welcome to the game of Murder and Mystery!');
+    let image = document.createElement('img');
+    image.src = 'assets/images.jpeg';
+    container.appendChild(image);
+    setTimeout(()=>{
+      container.removeChild(image);
+    },5000);
+  } else {
+    alert('We do not blame you for being scared to enter!');
+  }
+}
+quiz();
+
